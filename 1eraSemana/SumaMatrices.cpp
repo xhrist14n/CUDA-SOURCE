9c10a8035e74f7729891df37e3afb98c46f22c37
christian <xhrist14n@gmail.com>
2012-08-11 22:06:57 -0500

    cuda 1 er codigo en tbb

--- 1eraSemana/SumaMatrices.cpp
+++ 1eraSemana/SumaMatrices.cpp
@@ -1,8 +1,8 @@
-// cudaejemplo02.cpp: define el punto de entrada de la aplicación de consola.
+// SumaMatrices.cpp: define el punto de entrada de la aplicación de consola.
 // se tiene q copiar system 32 las dlls d bin
 
-#include <tbb/parallel_for.h> // esta libreria funciona en parelelo
-#include <tbb/tbb.h>
+#include <tbb/parallel_for.h> // esta libreria funciona en paralelo
+#include <tbb/tbb.h>		  // libreria TBB
 #include <ctime>
 #include <iostream>
 
@@ -10,18 +10,16 @@ using namespace tbb;
 using namespace std;
 class ProcesarSuma
 {
-private:
-	int *A;
-	int *B;
-	int *C;
-	int n;
+private: int *A;
+private: int *B;
+private: int *C;
+private: int n;
 
-public:
-	ProcesarSuma(int *_A,int*_B,int *_C,int n):A(_A),B(_B),C(_C),n(_n)
-	{
-		
-	}
-	void operator()(blocked_range<int>& block) const
+public: ProcesarSuma(int *_A,int*_B,int *_C,int n):A(_A),B(_B),C(_C),n(_n)
+		{
+			
+		}
+public: void operator()(blocked_range<int>& block) const
 	{
 		
 		for(int i=block.begin();i<block.end();i++)
@@ -34,25 +32,26 @@ public:
 	}
 };
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
+	srand(time(NULL));
 	time_t begin,end;
-	int size=1000000;
+	const int size=1000000;
 	int *A=new int[size];
 	int *B=new int[size];
 	int *C=new int[size];
 
 	for(int i=0;i<size;i++)
 	{	
-		A[i]=rand()%1000;
-		B[i]=rand()%1000;
+		A[i]=(rand()*1000)%1000;
+		B[i]=(rand()*1000)%1000;
 		C[i]=0;
 	
 	}
 
-	ProcesarSuma sum(A,B,C,size);
+	ProcesarSuma *sum=new ProcesarSuma(A,B,C,size);
 	begin=clock();
-	parallel_for(blocked_range<int>(0,size),sum);	
+	parallel_for(blocked_range<int>(0,size),*sum);	
 	end=clock();
 	cout<<(end-begin)*1.0f/CLOCKS_PER_SEC<<"\n";
 	
